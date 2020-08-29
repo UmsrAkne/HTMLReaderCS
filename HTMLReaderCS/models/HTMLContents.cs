06a0a8b6f939760b3d28323f4d4a45a1eabee2f2
Akn <sibata486@gmail.com>
2020-08-30 02:56:34 +0900

    クラス追加 / HTML文書を解析、保管するためのクラスを作成

---
+++ HTMLReaderCS/models/HTMLContents.cs
@@ -0,0 +1,19 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models {
+    class HTMLContents {
+
+        private IHtmlDocument htmlDocument;
+
+        public HTMLContents(String htmlText) {
+            var parser = new HtmlParser();
+            htmlDocument = parser.ParseDocument(htmlText);
+        }
+    }
+}
