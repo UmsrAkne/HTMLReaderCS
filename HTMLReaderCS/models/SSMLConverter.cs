f5650c0b38d9a772d3ea91cbcadcd4aa8a031365
Akn <sibata486@gmail.com>
2020-09-03 01:19:05 +0900

    クラス追加 / テキストをssmlに変換するためのクラスを追加

---
+++ HTMLReaderCS/models/SSMLConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models
+{
+    /// <summary>
+    /// テキストを SSML に変換する機能を提供します
+    /// </summary>
+    public class SSMLConverter {
+        public SSMLConverter() {
+
+        }
+    }
+}
---
+++ HTMLReaderCSTests/models/SSMLConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HTMLReaderCS.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models.Tests
+{
+    [TestClass()]
+    public class SSMLConverterTests
+    {
+        [TestMethod()]
+        public void SSMLConverterTest() {
+            SSMLConverter converter = new SSMLConverter();
+        }
+    }
+}
