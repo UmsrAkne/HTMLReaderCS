00735ed479ff29f0a833141d7d51626fa5cc978e
Akn <sibata486@gmail.com>
2021-04-12 01:03:36 +0900

    amazon polly を使用するため、awssdk パッケージをプロジェクトに追加

---
+++ HTMLReaderCS/models/PollyPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models {
+    class PollyPlayer : ITalker {
+        public event EventHandler TalkEnded;
+
+        public void ssmlTalk(string ssmlText) {
+            throw new NotImplementedException();
+        }
+
+        public void stop() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
