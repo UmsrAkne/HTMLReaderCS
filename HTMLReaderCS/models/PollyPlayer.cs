b45e1c3ab2a42c14d6a332c7077aa90e38429c0f
Akn <sibata486@gmail.com>
2021-04-13 05:07:20 +0900

    Amazon polly を使用して読み上げ音声ファイルを生成する機能を追加

--- HTMLReaderCS/models/PollyPlayer.cs
+++ HTMLReaderCS/models/PollyPlayer.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.Polly;
+using Amazon.Polly.Model;
 
 namespace HTMLReaderCS.models {
-    class PollyPlayer : ITalker {
+    public class PollyPlayer : ITalker {
+
+        public SSMLConverter SsmlConverter { get; set; } = new SSMLConverter();
+
+        private DirectoryInfo OutputDirectoryInfo { get; set; } = new DirectoryInfo("outputs");
+
+        private AmazonPollyClient PollyClient { get; set; }
+
         public event EventHandler TalkEnded;
 
-        public void ssmlTalk(string ssmlText) {
-            throw new NotImplementedException();
+        public PollyPlayer() {
+            var accKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
+            var secKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+            PollyClient = new AmazonPollyClient(accKey,secKey, Amazon.RegionEndpoint.APNortheast1 );
+
+            if (!OutputDirectoryInfo.Exists) {
+                OutputDirectoryInfo.Create();
+            }
+        }
+
+        public void ssmlTalk(string plainText) {
+
+            SsmlConverter.Text = plainText;
+            var ssmlText = SsmlConverter.getSSML();
+
+            var req = new SynthesizeSpeechRequest();
+            req.VoiceId = VoiceId.Takumi;
+            req.OutputFormat = OutputFormat.Mp3;
+            req.TextType = TextType.Ssml;
+            req.Text = ssmlText;
+
+            var response = PollyClient.SynthesizeSpeech(req);
+
+            var stream = response.AudioStream;
+
+            var outputFileName = DateTime.Now.ToString("yyyyMMddHHmmssff");
+            var filePath = $"{OutputDirectoryInfo.Name}\\{outputFileName}.mp3";
+
+            using (var output = new FileStream(filePath, FileMode.Create)) {
+                stream.CopyTo(output);
+                stream.Flush();
+                stream.Close();
+            }
         }
 
         public void stop() {
---
+++ HTMLReaderCSTests/models/PollyPlayerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HTMLReaderCS.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models.Tests {
+    [TestClass()]
+    public class PollyPlayerTests {
+        [TestMethod()]
+        public void PollyPlayerTest() {
+            var pollyPlayer = new PollyPlayer();
+
+        }
+
+        [TestMethod()]
+        public void ssmlTalkTest() {
+            var pp = new PollyPlayer();
+            pp.ssmlTalk("読み上げテキストです");
+        }
+    }
+}
