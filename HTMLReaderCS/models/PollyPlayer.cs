c5757e0961ea89e112c61ca0a559ac76c926b0aa
Akn <sibata486@gmail.com>
2021-04-13 23:05:24 +0900

    機能追加 / 読み込んだ html を全部読み上げられるよう実装
    解決できなかった問題を回避するため、奇妙な実装になっている。理由はコメントを参照

--- HTMLReaderCS/models/PollyPlayer.cs
+++ HTMLReaderCS/models/PollyPlayer.cs
@@ -8,6 +8,7 @@ using Amazon.Polly;
 using Amazon.Polly.Model;
 using System.Windows;
 using WMPLib;
+using System.Windows.Threading;
 
 namespace HTMLReaderCS.models {
     public class PollyPlayer : ITalker {
@@ -20,15 +21,43 @@ namespace HTMLReaderCS.models {
 
         private WindowsMediaPlayer WMP { get; set; } = new WindowsMediaPlayer();
 
+        private DispatcherTimer timer;
+
         public event EventHandler TalkEnded;
 
         public PollyPlayer() {
             var accKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
             var secKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
-            PollyClient = new AmazonPollyClient(accKey,secKey, Amazon.RegionEndpoint.APNortheast1 );
+            PollyClient = new AmazonPollyClient(accKey, secKey, Amazon.RegionEndpoint.APNortheast1);
 
             if (!OutputDirectoryInfo.Exists) {
                 OutputDirectoryInfo.Create();
+            }
+
+            WMP.PlayStateChange += WMP_PlayStateChange;
+
+            /*
+             * WMPによる音声の再生が終了した際、この timer をスタートさせる。
+             * そして、更に１秒後、このタイマーのイベントハンドラによって再生終了イベントを送出する。
+             *
+             * この実装の理由 : 
+             * WMP の再生終了から直接イベントを送出した場合、次の音声が再生されないため……。
+             * 本当に原因がわからないけど、時間差でイベントを飛ばせば正常に動くようなので、やむを得ずこのような実装にする。
+             * どうしてか本当にわからない……。
+            */
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += (sender, e) => {
+                TalkEnded(this,new EventArgs());
+
+                // timer は一回開始するごとに、一回だけ動作すればいいので、このメソッドが実行した時点で止める。
+                timer.Stop();
+            };
+        }
+
+        private void WMP_PlayStateChange(int NewState) {
+            if((int)WMP.playState == (int)WMPPlayState.wmppsMediaEnded) {
+                timer.Start();
             }
         }
 
