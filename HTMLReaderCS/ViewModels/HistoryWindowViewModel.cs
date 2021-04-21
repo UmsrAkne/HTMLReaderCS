727452a968a31b4a64c40da7b188432a90606b52
Akn <sibata486@gmail.com>
2021-04-21 23:22:40 +0900

    履歴ウィンドウに ListBox を追加し、履歴を閲覧できるところまで実装

--- HTMLReaderCS/ViewModels/HistoryWindowViewModel.cs
+++ HTMLReaderCS/ViewModels/HistoryWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using HTMLReaderCS.models;
+using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -7,10 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HTMLReaderCS.ViewModels {
-    class HistoryWindowViewModel : IDialogAware {
+    class HistoryWindowViewModel : BindableBase, IDialogAware {
         public string Title => "履歴";
 
         public event Action<IDialogResult> RequestClose;
+
+        private SQLiteHelper SQLiteHelper { get; set; }
+
+        public List<OutputFileInfo> OutputHistory { get => outputHistory; set => SetProperty(ref outputHistory, value); }
+        private List<OutputFileInfo> outputHistory;
 
         public bool CanCloseDialog() => true;
 
@@ -18,6 +25,8 @@ namespace HTMLReaderCS.ViewModels {
         }
 
         public void OnDialogOpened(IDialogParameters parameters) {
+            SQLiteHelper = new SQLiteHelper();
+            OutputHistory = SQLiteHelper.getHistories();
         }
 
         public DelegateCommand CloseWindowCommand {
