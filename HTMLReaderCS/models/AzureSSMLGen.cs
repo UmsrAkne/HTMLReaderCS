3e4eb0ac2e989163702a2578370a1841d8837604
Akn <sibata486@gmail.com>
2021-04-22 18:25:09 +0900

    クラス追加 / SSML生成クラスを追加

---
+++ HTMLReaderCS/models/AzureSSMLGen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models {
+    public class AzureSSMLGen {
+
+        public string getSSML() {
+            return "str";
+        }
+    }
+}
---
+++ HTMLReaderCSTests/models/AzureSSMLGenTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HTMLReaderCS.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models.Tests {
+    [TestClass()]
+    public class AzureSSMLGenTests {
+        [TestMethod()]
+        public void getSSMLTest() {
+            Assert.Fail();
+        }
+    }
+}
