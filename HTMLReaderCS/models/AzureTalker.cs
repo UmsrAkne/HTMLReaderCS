91ddaeefdbb0498782ac9dea887fce09f4bc3fa3
Akn <sibata486@gmail.com>
2021-04-23 22:22:20 +0900

    機能追加 / 出力した音声が読み上げられるよう実装

--- HTMLReaderCS/models/AzureTalker.cs
+++ HTMLReaderCS/models/AzureTalker.cs
@@ -3,11 +3,13 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
+using WMPLib;
 
 namespace HTMLReaderCS.models {
     public class AzureTalker : ITalker {
 
         public string OutputFileName { get; private set; }
+        private WindowsMediaPlayer WMP { get; } = new WindowsMediaPlayer();
 
         public event EventHandler TalkEnded;
 
@@ -24,7 +26,7 @@ namespace HTMLReaderCS.models {
         }
 
         public void stop() {
-            throw new NotImplementedException();
+            WMP.controls.stop();
         }
 
         private async Task talk(string ssml) {
@@ -41,8 +43,11 @@ namespace HTMLReaderCS.models {
             using (var synthesizer = new SpeechSynthesizer(config,audioConfig)) {
                 var ssmlGen = new AzureSSMLGen();
                 await synthesizer.SpeakSsmlAsync(ssml);
+
             }
 
+            WMP.URL = $"{OutputDirectoryInfo.Name}\\{OutputFileName}";
+            WMP.controls.play();
         }
 
     }
