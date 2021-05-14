a87325c825eda581c0ef07fb06b7488641c2795d
Akn <sibata486@gmail.com>
2021-05-15 00:29:31 +0900

    Player の要件を定義するインターフェース IPlayer を追加。

---
+++ HTMLReaderCS/models/IPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLReaderCS.models {
+    public interface IPlayer {
+        ObservableCollection<FileInfo> FileList { get; set; }
+        FileInfo SelectedFile { get; set; }
+        int SelectedFileIndex { get; set; }
+        int SelectedTextIndex { get; set; }
+
+        DelegateCommand PlayCommand { get; }
+        DelegateCommand StopCommand { get; }
+
+    }
+}
