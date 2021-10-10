79bf52ad8b3d9993d18f824f60bd61240ffc1af9
Akn <sibata486@gmail.com>
2021-10-11 01:31:59 +0900

    add / 読み上げるテキストの要素を表すクラスを追加

---
+++ HTMLReaderCS/models/LineText.cs
@@ -0,0 +1,13 @@
+﻿namespace HTMLReaderCS.Models
+{
+    public class LineText
+    {
+        public string Text { get; set; }
+
+        public int LineNumber { get; set; }
+
+        public bool IsSelected { get; set; }
+
+        public bool IsEmpty => string.IsNullOrWhiteSpace(Text);
+    }
+}
